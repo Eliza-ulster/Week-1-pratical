05450adf0f164cbc67bb14132c9b6e5aad7bc232
Eliza Ann Benoy <Eliza_Ann_Benoy-EAB@ulster.ac.uk>
2024-10-01 11:56:16 +0100

    age

--- Week1pratical/Program.cs
+++ Week1pratical/Program.cs
@@ -1,1 +1,19 @@
-﻿int firstnumber = 3;
+﻿
+Age();
+
+
+
+void Age()
+{
+
+    Console.WriteLine("Please enter your name :");
+    String name = Console.ReadLine();
+    Console.WriteLine("Please enter your age :");
+
+    int age = Convert.ToInt32(Console.ReadLine());
+    age += 5;
+
+
+    Console.WriteLine("Hello " + name + " you will be " + age + " in 5 years ");
+
+}
