11f3d7070c5ceec131fcd3ce26a3e2b06cc861b6
Eliza Ann Benoy <Eliza_Ann_Benoy-EAB@ulster.ac.uk>
2024-10-07 19:04:31 +0100

    age2

--- Week1pratical/Program.cs
+++ Week1pratical/Program.cs
@@ -17,3 +17,29 @@ void Age()
     Console.WriteLine("Hello " + name + " you will be " + age + " in 5 years ");
 
 }
+
+UserAge();
+
+void UserAge()
+{
+    Console.WriteLine("Please enter your age :");
+    int age = Convert.ToInt32(Console.ReadLine());
+    if (age < 13)
+    {
+        Console.WriteLine(" You are a Child");
+    }
+    else if (age >= 13 && age <=19)
+    {
+        Console.WriteLine("You are a teenager");
+
+
+    }
+    else if (age <=110)
+    {
+        Console.WriteLine("You are old");
+    }
+    else
+    {
+        Console.WriteLine("You are entered a invaild age ")
+    }
+}
