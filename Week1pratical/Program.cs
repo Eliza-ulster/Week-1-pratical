8ba43e62971ecd56086d4470afae3c3a707bc042
Eliza Ann Benoy <Eliza_Ann_Benoy-EAB@ulster.ac.uk>
2024-10-07 19:30:06 +0100

    final

--- Week1pratical/Program.cs
+++ Week1pratical/Program.cs
@@ -43,3 +43,66 @@ void UserAge()
         Console.WriteLine("You are entered a invaild age ")
     }
 }
+
+Integers();
+void Integers();
+{
+    Console.WriteLine("Please enter an interger");
+
+    int Num = Convert.ToInt32(Console.ReadLine());
+    if (Num ==0)
+    {
+        Console.WriteLine("This value equals 0");
+    }
+    else if(Num >0)
+    {
+        Console.WriteLine("This is a postive integer");
+    }
+    else if (Num <0)
+    {
+        Console.WriteLine("This a negative integer");
+    }
+}
+
+LeapYear();
+ 
+
+void LeapYear()
+{
+    Console.WriteLine("Enter a year");
+    int year = Convert.ToInt32(Console.ReadLine());
+    if (year < 0)
+    {
+        Console.WriteLine($"{year} is not a vaild year. it must not be a negative number .");
+
+    }
+    else
+    {
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+        {
+            Console.WriteLine($"{year} is a leap year");
+        
+        }
+        else
+        {
+            Console.WriteLine($"{year} is not a leap year");
+        }
+    }
+
+}
+Numbers();
+
+void Numbers()
+{
+    Console.WriteLine("Enter a number:");
+    int num = Convert.ToInt32(Console.ReadLine());
+    if (num %== 2)
+    {
+        Console.WriteLine("This ia an even number");
+
+    }
+    else
+    {
+        Console.WriteLine("This is an odd number");
+    }
+}
